5cf5044de08d6046a95300d5ec9f54815eba9476
davidmueller13 <david.mueller1013@gmail.com>
2017-05-21 21:01:56 +0200

    add yankactive gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -483,6 +483,9 @@ extern struct cpufreq_governor cpufreq_gov_alucard;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
+extern struct cpufreq_governor cpufreq_gov_yankactive;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_yankactive)
 #endif
 
 
