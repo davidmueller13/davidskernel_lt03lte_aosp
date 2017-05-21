f53e6c7b16f31d54e6982927ff0970c95e75f3bd
davidmueller13 <david.mueller1013@gmail.com>
2017-05-21 20:50:56 +0200

    add smartass2 gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -480,6 +480,9 @@ extern struct cpufreq_governor cpufreq_gov_bioshock;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_alucard)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass2)
 #endif
 
 
